c1c6673267a00668afdf024924a7d73cf16d72a6
PeterBin <hslinxubin@126.com>
2013-04-26 20:03:19 +0800

    第四此

--- ListItem.h
+++ ListItem.h
@@ -12,7 +12,7 @@ public:
 	char * title;
 
 	//0x04 用来保存ReponseItem.Item2以后的内容
-	ContextItem * pContextItem;
+	CDARecord * pRecord;
 
 	//0x08,客户端的时间戳
 	Time TimeStamp;
@@ -43,7 +43,10 @@ public :
 	//0x0,长度 item2以后的缓存区的大小
 	int Len;
 	//0x4
-	int item2;
+	short item2;
+
+	//0x6,两个字节,CID列的内容
+	char[2] CID;
 
 	//0x08
 	int item3;
@@ -57,20 +60,65 @@ public :
 
 
 //
-class ContextItem
+class CDARecord
 {
 public :
 	//0x0,长度 为 ContextItem 不包含Served字段的长度,也就是 Len = ContextItem -0xCC
 	int Len;
 	//0x4
 	int item2;
+	
+	//0x6,两个字节,CID列的内容
+	char[2] CID;
 
 	//0x08
 	int item3;
 
+	//0xB
+	byte item3_1;
+
+
 	//0x0C
 	char[] Title;
+
+	//0x未知
+	char[] NONE;
+
+	//0x未知
+	char[] Item4;
+
+	//四个字节 上一次修改的时间
+	Time LastChanged
+
+	char[] Value;
+
+	
+
+
+
+	char[] Context;
 
 	//保留的字符串 0xCC
 	char[]  served;
 }
+
+
+public class CMainDialog
+{
+public:
+	///简易列表名称
+	/// 0x98
+	List<String> MnemonicsList;
+
+	//0x80,记录集合列表 
+	CDARecordList *RecordList;
+
+	//0x24C,显示数据的表格控件
+	GridView Control_Grid;
+}
+
+
+//CID 列 类型
+//ZO 为时间
+//ID 为字符串
+
